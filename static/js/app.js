0de7be949125627ab4e54379187220015a6b831c
fruitCo <rhoadesmccarthy@gmail.com>
2022-12-07 16:11:59 +0100

    third comit

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,30 @@
+// const simpleParallax = require('simple-parallax-js');
+
+(function () {
+    var parallax = $('.js-parallax');
+    console.log(parallax.length);
+
+    if (parallax.length) {
+        parallax.each(function () {
+            var _this = $(this),
+                scale = _this.data('scale'),
+                orientation = _this.data('orientation');
+
+            new simpleParallax(_this[0], {
+                scale: scale,
+                orientation: orientation,
+                delay: .5,
+                overflow: false,
+                transition: 'cubic-bezier(0,0,0,1)'
+            });
+        });
+    }
+})();
+
 // initialize aos
 AOS.init({
-    once: false
+    once: false,
+    // ffset: 80,
 });
 
 // hero section scroll 
@@ -50,7 +74,7 @@ const swiper1 = new Swiper('.swiper1', {
         delay: 5000,
     },
 
-    slidesPerView: 3,
+    slidesPerView: window.innerWidth < 1279 ? 1 : 2,
     grid: {
         rows: 1,
     },
@@ -67,6 +91,18 @@ const swiper1 = new Swiper('.swiper1', {
     // },
 });
 
+function scrollRemove() {
+    var header = $('.js-header'),
+        burger = header.find('.js-header-burger'),
+        wrapper = header.find('.js-header-wrapper'),
+        html = $('html'),
+        body = $('body');
+    header.removeClass('active');
+    burger.removeClass('active');
+    wrapper.removeClass('visible');
+    html.removeClass('no-scroll');
+    body.removeClass('no-scroll');
+}
 
 // scroll to section
 (function () {
@@ -77,6 +113,42 @@ const swiper1 = new Swiper('.swiper1', {
         }, {
             duration: 1000,
         });
+        scrollRemove()
         return false;
     });
 })();
+
+
+// toggle header
+// header
+(function () {
+    var header = $('.js-header'),
+        burger = header.find('.js-header-burger'),
+        wrapper = header.find('.js-header-wrapper'),
+        html = $('html'),
+        body = $('body');
+    burger.on('click', function () {
+        header.toggleClass('active');
+        burger.toggleClass('active');
+        wrapper.toggleClass('visible');
+        html.toggleClass('no-scroll');
+        body.toggleClass('no-scroll');
+    });
+})(); 
+
+
+// navbar
+document.addEventListener("DOMContentLoaded", function(){
+    window.addEventListener('scroll', function() {
+        if (window.scrollY > 50) {
+          document.getElementById('header').classList.add('fixed-top');
+          // add padding top to show content behind navbar
+          navbar_height = document.querySelector('#header').offsetHeight;
+          document.body.style.paddingTop = navbar_height + 'px';
+        } else {
+          document.getElementById('header').classList.remove('fixed-top');
+           // remove padding top from body
+          document.body.style.paddingTop = '0';
+        } 
+    });
+  }); 
